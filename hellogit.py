0a72ee45258a510a190fbda14189b0795243087d
Nahuel <cristian.nina@uab.edu.bo>
2024-02-16 19:02:35 -0400

    commit 7

--- hellogit.py
+++ hellogit.py
@@ -12,6 +12,6 @@ print("¡Hola git! with a change")
 
 #git checkout "hash" ->para volver a un estado anterior y se eliminara todo lo que hiciste hasta ese commit
 
-#git tag  es para etiquetar un commit 
+#git tag (nombre etiqueta) es para etiquetar un commit 
 
-#git checkout tags/etiqueta 
+#git checkout tags/nombre etiqueta 
